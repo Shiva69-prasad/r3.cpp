50f5e52cacab58ccb8d47ec15b68bf2aedae09ec
Shiva69-prasad <shiva31694@gmail.com>
2025-10-07 22:23:51 +0530

    r3.cpp

---
+++ r3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Solution{
+    public:
+    void printRightTriangle(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                cout<<"  ";    //it contain 2space
+            }
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+    }
+};
+int main(){
+    Solution obj;
+    int n;
+    cout<<"enter an number:";
+    cin>>n;
+    obj.printRightTriangle(n);
+    return 0;
+}
